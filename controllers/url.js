2f6a2106bcaba1f58099a03e45b255d806767319
FenilAhir98980 <backupsystem9909@gmail.com>
2025-03-07 04:01:50 -0800

    Finishing some comments for the code

--- controllers/url.js
+++ controllers/url.js
@@ -1,10 +1,12 @@
 
+// This constant nanoid is holding the package shortid which would be helping us to create an short ID.
 const nanoid = require('shortid');
 
 const urlmodel = require('../models/url');
-const shortid = require('shortid');
 
 
+// In this function we would be checking for the url not to be empty if it is not empty then we would be createe a short ID first using the shortid package and then pushing
+// the shortID, redirectURL inside mongoDB.
 async function GenerateshortURL(request, response){
     const body = request.body;
     if(!body.url){
@@ -20,6 +22,9 @@ async function GenerateshortURL(request, response){
 
 }
 
+// This function would be helping us to handle the redirect mechanism, so we would be first getting the shortID from the shorten URL and then would be finding that shortID,
+// from the mongodb, once we get that shortID we would be updating the visithistory by entering the timestamp, meaning when was the url like the shorturl accessed. Once this is done
+// then we would be getting the original url associated with the shortID and redirecting the user to that original url again.
 async function HandleRedirect(request, response){
     const shortId= request.params.shortId
     const entry = await urlmodel.findOneAndUpdate({
@@ -34,6 +39,8 @@ async function HandleRedirect(request, response){
     response.redirect(entry.redirectURL);
 }
 
+// This function is quite simple, it is helping us to know how many times the url was clicked which is straight forward because we are just going to return the length
+// of the visithistory array which is holding the timestamps which are created when the user sends a request using the new generated short url.
 async function GetAnalytics(request, response){
  const shortId = request.params.shortId;
   const siteobject = await urlmodel.findOne({
--- routes/url.js
+++ routes/url.js
@@ -6,8 +6,11 @@ const controllerhelp = require('../controllers/url');
 
 application.use(express.json());
 
+// This route would be helping us to get the shortID and enter it inside the database.
 application.post('/url',controllerhelp.GenerateshortURL);
+// This would be matching the shortID which is present in the url provided by the user and then performing the redirect logic.
 application.get('/:shortId',controllerhelp.HandleRedirect);
+// This route would be helping us to get the analytics over a URL, helping us to know how many times a URL was clicked and the timestamps as well.
 application.get('/url/analytics/:shortId',controllerhelp.GetAnalytics);
 
 module.exports = application;
